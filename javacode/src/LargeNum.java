6d22b569e0b0ac531c2186234ebd9bcccc8ba70e
Nazim369 <108105390+Nazim369@users.noreply.github.com>
2023-12-20 20:08:15 -0700

    Initial commit

---
+++ javacode/src/LargeNum.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class LargeNum {
+
+    public static void main(String[] args) {
+
+           // int[] numbers = { 10, 25, 5, 40, 35, 30 };
+
+            // Sort the array in descending order
+           // Arrays.sort(numbers);
+           // int largestNumber = numbers[numbers.length - 1];
+           // int secondLargestNumber = numbers[numbers.length - 2];
+
+
+       // int smallestNumber = numbers[0];
+       // int secondSmallestNumber = numbers[1];
+
+       // System.out.println("Smallest number: " + smallestNumber);
+       // System.out.println("Second smallest number: " + secondSmallestNumber);
+
+           // System.out.println("Largest number: " + largestNumber);
+           // System.out.println("Second largest number: " + secondLargestNumber);
+        if(System.out.printf("Java Language")==null){
+
+        }
+    }
+        }
+    
+
---
+++ javacode/src/Test.java
@@ -0,0 +1,16 @@
+public class Test {
+
+
+
+    void test(){
+        double x = 0.1;
+        double y = 0.2;
+        double z;
+       double x+y =z;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Test.test);
+    }
+
+}
